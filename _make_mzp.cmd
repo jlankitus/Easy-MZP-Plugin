60018105b006269770ccacc9fea98422f4e0365a
jed <jlankitus@handelarchitects.com>
2017-10-27 13:55:16 -0400

    first commit with working plugin

---
+++ _make_mzp.cmd
@@ -0,0 +1,6 @@
+@REM Only a small batch file to create the MAXScript ZIP Package (*.mzp File)
+
+@set filename=MAXScript_ZIP_Package\MZP_Plugin.mzp
+del %filename%
+"%ProgramFiles%\7-Zip\7z.exe" u -tzip -mx9 %filename% *.ms *.mcr *.run *.txt pythonScript.py
+@pause
---
+++ pythonScript.py
@@ -0,0 +1,4 @@
+import MaxPlus, pymxs
+from pymxs import runtime as rt
+
+print "python script called!"
