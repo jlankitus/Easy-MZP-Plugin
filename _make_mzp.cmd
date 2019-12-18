dbb8da5916752f63dd2fa4221cc36109530d21e8
OrionScripts <59019352+OrionScripts@users.noreply.github.com>
2019-12-18 16:31:13 -0500

    Initial Commit with Expanded Feature Set
    Adds the option for controlling various variables and directories in a slightly automated fashion. Also adds the new ability for initializing scripts that require entire directories to be copied and initialized in various locations on user systems.

--- _make_mzp.cmd
+++ _make_mzp.cmd
@@ -1,6 +1,7 @@
 @REM Only a small batch file to create the MAXScript ZIP Package (*.mzp File)
 
-@set filename=MAXScript_ZIP_Package\MZP_Plugin.mzp
+@REM (Edit your MZP name here.)
+@set filename=MAXScript_ZIP_Package\OrionScripts-Github.mzp
 del %filename%
 "%ProgramFiles%\7-Zip\7z.exe" u -tzip -mx9 %filename% *.ms *.mcr *.run *.txt pythonScript.py
 @pause
