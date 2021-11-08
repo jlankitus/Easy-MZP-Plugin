0822b7f8fbcc74618a4698021d80d532261a0134
OrionScripts <59019352+OrionScripts@users.noreply.github.com>
2021-11-08 13:44:12 -0500

    Add files via upload
    Updating file repository, fixing issues, updated for Max 2022.

--- _make_mzp.cmd
+++ _make_mzp.cmd
@@ -1,7 +1,6 @@
 @REM Only a small batch file to create the MAXScript ZIP Package (*.mzp File)
 
-@REM (Edit your MZP name here.)
-@set filename=MAXScript_ZIP_Package\OrionScripts-Github.mzp
+@set filename=MAXScript_ZIP_Package\Orion.mzp
 del %filename%
 "%ProgramFiles%\7-Zip\7z.exe" u -tzip -mx9 %filename% *.ms *.mcr *.run *.txt pythonScript.py
 @pause
